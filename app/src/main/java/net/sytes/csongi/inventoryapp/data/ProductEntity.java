ca5531a3b1950bbbd4a314699b471c41a33cb77a
Horváth Csongor <horvath.csongor.3d@gmail.com>
2018-06-05 13:39:53 +0200

    add docs for ProductEntity

--- app/src/main/java/net/sytes/csongi/inventoryapp/data/ProductEntity.java
+++ app/src/main/java/net/sytes/csongi/inventoryapp/data/ProductEntity.java
@@ -10,9 +10,16 @@ public class ProductEntity implements Parcelable {
     private int mId, mPrice, mQuantity;
     private String mProductName, mSupplierName, mSupplierPhone;
 
+    /**
+     * An emtpy Ctor
+     */
     public ProductEntity() {
     }
 
+    /**
+     * Parcelable implementation for this entity
+     * @param in
+     */
     protected ProductEntity(Parcel in) {
         mId = in.readInt();
         mPrice = in.readInt();
@@ -34,14 +41,27 @@ public class ProductEntity implements Parcelable {
         }
     };
 
+    /**
+     * Getter for entity's Id
+     * @return the id
+     */
     public int getId() {
         return mId;
     }
 
+    /**
+     * Since only EntityManager can access to database and none of the clients can
+     * set this value directly, the setter remains package private
+     * @param id the id of Entity which was retreived from database
+     */
     void setId(int id) {
         this.mId=id;
     }
 
+    /**
+     * Getter and setter for product price
+     * @return int value of price
+     */
     public int getPrice() {
         return mPrice;
     }
@@ -50,6 +70,10 @@ public class ProductEntity implements Parcelable {
         this.mPrice = price;
     }
 
+    /**
+     * Getter and setter for Quantity of product
+     * @return int value of quantity
+     */
     public int getQuantity() {
         return mQuantity;
     }
@@ -58,6 +82,10 @@ public class ProductEntity implements Parcelable {
         this.mQuantity = quantity;
     }
 
+    /**
+     * Getter and setter for product Name
+     * @return String - name of product
+     */
     public String getProductName() {
         return mProductName;
     }
@@ -66,6 +94,10 @@ public class ProductEntity implements Parcelable {
         this.mProductName = productName;
     }
 
+    /**
+     * Getter and setter for Supplier Name
+     * @return String - Name of supplier
+     */
     public String getSupplierName() {
         return mSupplierName;
     }
@@ -74,6 +106,11 @@ public class ProductEntity implements Parcelable {
         this.mSupplierName = supplierName;
     }
 
+    /**
+     * Getter and setter for Supplier phone. Since there might be other characters than digits,
+     * this field type is String
+     * @return String - Phone number of Supplier
+     */
     public String getSupplierPhone() {
         return mSupplierPhone;
     }
@@ -82,6 +119,10 @@ public class ProductEntity implements Parcelable {
         this.mSupplierPhone = supplierPhone;
     }
 
+    /**
+     * Parcelable implementation
+     * @return
+     */
     @Override
     public int describeContents() {
         return 0;
