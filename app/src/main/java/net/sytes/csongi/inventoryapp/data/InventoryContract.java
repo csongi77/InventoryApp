2b79cb80c4af49c1fcc1de1ebd4368e645a4d4af
Horváth Csongor <horvath.csongor.3d@gmail.com>
2018-06-21 09:26:56 +0200

    Add iInventoryProvider

--- app/src/main/java/net/sytes/csongi/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/net/sytes/csongi/inventoryapp/data/InventoryContract.java
@@ -1,11 +1,19 @@
 package net.sytes.csongi.inventoryapp.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Contract for products
  */
 final class InventoryContract {
+
+    // defining constants: content authority, base content uri and path for Products and Suppliers
+    private static final String CONTENT_AUTHORITY="net.sytes.csongi.inventoryapp";
+    private static final Uri BASE_CONTENT_URI=Uri.parse("content://"+CONTENT_AUTHORITY);
+    private static final String PATH_PRODUCTS="products";
+    private static final String PATH_SUPPLIERS="suppliers";
 
     // empty constructor for avoiding instantiating this class
     private InventoryContract() {
@@ -14,8 +22,12 @@ final class InventoryContract {
     // the product table contract
     static class ProductEntry implements BaseColumns {
 
-        // defining columns
+        // defining table name
         public static final String TABLE_NAME="products";
+
+
+
+        // defining columns
         public static final String _ID=BaseColumns._ID;
         public static final String COLUMN_NAME_PRODUCT_NAME ="product_name";
         public static final String COLUMN_NAME_PRICE ="price";
---
+++ app/src/main/java/net/sytes/csongi/inventoryapp/data/InventoryProvider.java
@@ -0,0 +1,53 @@
+package net.sytes.csongi.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class InventoryProvider extends ContentProvider {
+
+    private InventoryDbHelper mInventoryDbHelper;
+    private static UriMatcher sMatcher=new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+
+    }
+
+    @Override
+    public boolean onCreate() {
+        mInventoryDbHelper = new InventoryDbHelper(getContext());
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
